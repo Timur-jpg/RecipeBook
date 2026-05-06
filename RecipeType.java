b092448d22c9326dae4848c774b150e4e53e89c1
Timur-jpg <timur.turdaliev08@gmail.com>
2026-05-06 04:15:53 -0400

    Initial sharing of project

---
+++ RecipeType.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class RecipeType - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum RecipeType
+{
+    DESSERT, MAIN_COURSE, APPETIZER, BEVERAGE, BREAKFAST
+}
---
+++ Tag.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Tag - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Tag
+{
+    VEGETARIAN, VEGAN, GLUTEN_FREE, QUICK, SPICY, DAIRY_FREE
+}
