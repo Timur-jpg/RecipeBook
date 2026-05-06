7ef0c256233a2f742b3b6dff5a030fba14d8b678
Timur-jpg <timur.turdaliev08@gmail.com>
2026-05-06 04:47:50 -0400

    Arrows

--- BakingRecipe.java
+++ BakingRecipe.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class BakingRecipe
+public class BakingRecipe extends Recipe
 {
     // instance variables - replace the example below with your own
     private int x;
--- DrinkRecipe.java
+++ DrinkRecipe.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class DrinkRecipe
+public class DrinkRecipe extends Recipe
 {
     // instance variables - replace the example below with your own
     private int x;
